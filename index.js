95fd7d521cd60251e2b8747eb774ce9403ac2e79
perillamint <perillamint@gentoo.moe>
2016-06-20 03:10:32 +0900

    Strict compare

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ class MiScale extends EventEmitter {
         //Parse service data.
         let svcData = scale.svcData;
 
-        scale.isStabilized = ((svcData[0] & (1<<5)) != 0) ? true : false;
-        scale.loadRemoved = ((svcData[0] & (1<<7)) != 0) ? true : false;
+        scale.isStabilized = ((svcData[0] & (1<<5)) !== 0) ? true : false;
+        scale.loadRemoved = ((svcData[0] & (1<<7)) !== 0) ? true : false;
 
-        if((svcData[0] & (1<<4)) != 0) { // Chinese Catty
+        if((svcData[0] & (1<<4)) !== 0) { // Chinese Catty
             scale.unit = "jin";
-        } else if ((svcData[0] & 0x0F) == 0x03) { // Imperial pound
+        } else if ((svcData[0] & 0x0F) === 0x03) { // Imperial pound
             scale.unit = "lbs";
-        } else if ((svcData[0] & 0x0F) == 0x02) { // MKS kg
+        } else if ((svcData[0] & 0x0F) === 0x02) { // MKS kg
             scale.unit = "kg";
         } else {
             throw new Error("Invalid data!");
@@ -42,24 +42,23 @@ class MiScale extends EventEmitter {
 
         scale.weight = svcData.readUInt16LE(1) / 100;
 
-        if(scale.unit == "kg") { //Convert chinese Catty to kg.
+        if(scale.unit === "kg") { //Convert chinese Catty to kg.
             scale.weight /= 2;
         }
 
-        console.log(scale);
         this.emit('data', scale);
     };
 
     _nobleDiscoverListener(peripheral) {
         if(this._macAddr != undefined) {
             if(peripheral.address == this._macAddr &&
-               peripheral.advertisement.localName == "MI_SCALE") {
+               peripheral.advertisement.localName === "MI_SCALE") {
                 // Matched!
                 this._scaleListener(peripheral);
             }
         } else {
             // Match any Xiaomi scale and send event.
-            if(peripheral.advertisement.localName == "MI_SCALE") {
+            if(peripheral.advertisement.localName === "MI_SCALE") {
                 //Matched!
                 this._scaleListener(peripheral);
             }
